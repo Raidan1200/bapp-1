3135eee96dc4a754a45d08ebf1bf0b8e568444e7
Oliver Schumacher <15702230+birusch@users.noreply.github.com>
2021-10-12 17:09:43 +0200

    Add package and item lists to PDF

--- app/Services/Pdf.php
+++ app/Services/Pdf.php
@@ -31,11 +31,16 @@ class Pdf
         $this->drawKnicklinien();
         $this->writeKontakt(140, 48);
         $this->writeBank(140, 70);
-        // $this->writeKopf();
+        $this->writeKopf();
         $this->writeAnschrift();
         $this->writeBetreff();
-        // $this->drawPositionen();
+        $this->drawPositionen();
         $this->writeFuss();
+    }
+
+    public function output($type = 'I', $dest = '')
+    {
+        return $this->pdf->output($type, $dest);
     }
 
     private function drawKnicklinien()
@@ -92,11 +97,11 @@ class Pdf
         $this->pdf->write(5, "BIC:");
     }
 
-    private function writeKopf() // TODO
+    private function writeKopf()
     {
         $this->pdf->setFont("Arial","", 7);
         $this->pdf->setY(46);
-        $this->pdf->write(10, utf8_decode($this->text->getText('rechnung', 'kopf')));
+        $this->pdf->write(10, utf8_decode($this->order->venue->invoice_blocks['company']));
     }
 
     private function writeAnschrift()
@@ -157,177 +162,88 @@ class Pdf
         $this->pdf->setFont("Arial","", 7);
         $i = 1;
         $gesamt = 0;
-        // unterschiedliche Mehrwertsteuersätze
-        $mwst7 = 1.05;
-        $anteilgering = 0;
-        $mwst19 = 1.16;
-        $anteilvoll = 0;
-        foreach ($this->reservierung->getPositionen() as $position) {
-            // Sonderfall: Extra Kosten werden voll Berechnet
-            $bezeichnung_zusatz = '';
 
-            if ($position->getOrt() == 'extra') {
-                $einzelpreis = $position->getEinzelpreisBrutto();
-            } else {
-                $einzelpreis = round($position->getEinzelpreisBrutto(), 2);
-            }
-
-            $summe = $einzelpreis * $position->getAnzahl();
-
-
-            // Extra percent info
-            if ($this->prozent === 20) {
-                $bezeichnung_zusatz = ' - '.$this->prozent.' %';
-                $summe = $einzelpreis * $position->getAnzahlbak();
-            }
-
-            // Show flat option
-            $bezeichnung = $position->getBezeichnung();
-            if ($position->getFlat() === 'yes') {
-                $bezeichnung .= ' + GF';
-            }
-
+        foreach ($this->order->bookings as $booking) {
             $this->pdf->ln();
             $this->pdf->write(6, $i++);
             $this->pdf->setX(33);
-            $this->pdf->write(6, utf8_decode(strtoupper(substr($position->getOrt(), 0, 3).substr($position->getArt(), 0, 3))));
+            $this->pdf->write(6, utf8_decode('ART NR?')); // TODO
             $this->pdf->setX(50);
-            $this->pdf->write(6, utf8_decode($bezeichnung) . $bezeichnung_zusatz);
+            $this->pdf->write(6, utf8_decode($booking->package_name));
             $this->pdf->setX(96);
-            $this->pdf->write(6, utf8_decode($position->getDatum()->format('d.m.Y').' '.$position->getVon().' - '.$position->getBis().' Uhr'));
-            // leider Sonderfall bei der Exklusiven Einmietung soll es keine Anzahl und Einzelpreis geben
-            if ($position->getArt() != 'paket5') {
-                $this->pdf->setX(128);
-                if ($this->prozent === 20) {
-                    $this->pdf->write(6, utf8_decode($position->getAnzahlbak()));
-                } else {
-                    $this->pdf->write(6, utf8_decode($position->getAnzahl()));
-                }
-                $this->pdf->setX(141);
-                if ($this->prozent === 20) {
-                    $this->pdf->write(6, utf8_decode(number_format($einzelpreis / 100 * 20, 2, ',', '.').' Euro'));
-                } else {
-                    $this->pdf->write(6, utf8_decode(number_format($einzelpreis, 2, ',', '.').' Euro'));
-                }
-            }
+            $this->pdf->write(6, utf8_decode($booking->starts_at->format('d.m.Y H:i').'-'.$booking->ends_at->format('H:i')));
+            $this->pdf->setX(128);
+            $this->pdf->write(6, utf8_decode($booking->quantity));
+            $this->pdf->setX(141);
+            $this->pdf->write(6, utf8_decode(money($booking->unit_price).' Euro'));
             $this->pdf->setX(167);
-
-            if ($this->prozent === 20) {
-                $summe = $summe / 100 * 20;
-            }
-             /** Unterschiedliche Steuer Anpassung*/
-            //Paket 1 = Hüttenzauberclassic
-            //Paket 2  = Premium mit Getränkeflat
-            if ($position->getArt() == 'paket1') {
-              $anteilgering = $anteilgering + ($summe - $summe / $mwst7);
-
-            } elseif ($position->getArt() == 'paket2'){
-              //58.9 - 33.00 = 25.90  - 25.90 / 1.07
-              if ($this->prozent === 20) {
-                $preisgering = ($einzelpreis - 30.40) / 100 * 20;
-                $preisvoll = ($einzelpreis - 28.50) /100 * 20;
-                $anteilgering = $anteilgering + ($preisgering * $position->getAnzahl() - ($preisgering * $position->getAnzahl()) / $mwst7); //$summe/$mwst7;
-                $anteilvoll = $anteilvoll + ($preisvoll * $position->getAnzahl() - ($preisvoll * $position->getAnzahl()) / $mwst19);
-              } else {
-                /* $anteilgering = $anteilgering + (($einzelpreis - 30.40) * $position->getAnzahl() - (($einzelpreis - 30.40) * $position->getAnzahl()) / $mwst7); //$summe/$mwst7; */
-                /* $anteilvoll = $anteilvoll + (($einzelpreis - 28.50) * $position->getAnzahl() - (($einzelpreis - 28.50) * $position->getAnzahl()) / $mwst19); */
-                $anteilgering = $anteilgering + (($summe - 30.40 * $position->getAnzahl()) - (($summe - (30.40 * $position->getAnzahl())) / $mwst7)); //$summe/$mwst7;
-                $anteilvoll = $anteilvoll + (($summe - 28.50 * $position->getAnzahl()) - (($summe - (28.50 * $position->getAnzahl())) / $mwst19));
-              }
-            } else {
-              $anteilvoll = $anteilvoll + ($summe - $summe / $mwst19);
-            }
-
-
-            $this->pdf->write(6, utf8_decode(number_format($summe, 2, ',', '.').' Euro'));
-            $gesamt = $gesamt + $summe;
-        }
-        $gesamt = $gesamt;
-        // wenn es Änderungen gab, kann die Anzahlung abgezogen werden
-
-        if ($this->reservierung->getAnzahlung() > 0 && $this->prozent !== 20 && $this->reservierung->getGesamtrechnung() === 0) {
-            $verrechnung = $this->reservierung->getAnzahlung() * -1;
-            $this->pdf->ln();
-            $this->pdf->write(6, $i);
-            $this->pdf->setX(33);
-            $this->pdf->write(6, utf8_decode('VERANZ'));
-            $this->pdf->setX(50);
-            $this->pdf->write(6, utf8_decode('Verrechnung Anzahlung'));
-            if ($verrechnung < 0) {
-                $this->pdf->setX(166);
-            } else {
-                $this->pdf->setX(167);
-            }
-            $this->pdf->write(6, utf8_decode(number_format($verrechnung, 2, ',', '.').' Euro'));
-            $gesamt = $gesamt + $verrechnung;
+            $this->pdf->write(6, utf8_decode(money($booking->grossTotal).' Euro'));
         }
 
-        $this->betrag = $gesamt;
+        $this->pdf->ln();
+        $this->pdf->write(6, $i);
+        $this->pdf->setX(33);
+        $this->pdf->write(6, utf8_decode('VERANZ'));
+        $this->pdf->setX(50);
+        $this->pdf->write(6, utf8_decode('Verrechnung Anzahlung TODO'));
+        // if ($verrechnung < 0) {
+            $this->pdf->setX(166);
+        // } else {
+        //     $this->pdf->setX(167);
+        // }
+        $this->pdf->write(6, utf8_decode('000' .' Euro'));
+
         $this->pdf->ln();
         $this->pdf->cell(170, 0, "", true);
         $this->pdf->ln();
         $this->pdf->setX(141);
         $this->pdf->write(5, "Gesamt netto");
         $this->pdf->setX(167);
-        // Nettopreis ausgeben
-        // $this->pdf->write(5, number_format($gesamt / 1.19, 2, ',', '.').' Euro');
-         $this->pdf->write(5, number_format($gesamt - $anteilgering - $anteilvoll, 2, ',', '.').' Euro');
-        //neu anteilig
-        if($anteilgering !== 0){
-        $this->pdf->ln();
-        $this->pdf->setX(141);
-        $this->pdf->write(5, "5% MwSt.");
-        $this->pdf->setX(167);
-        $this->pdf->write(5, number_format($anteilgering, 2, ',', '.').' Euro');
+        $this->pdf->write(5, money($this->order->netTotal).' Euro');
+
+        foreach ($this->order->vats as $vat => $amount) {
+            $this->pdf->ln();
+            $this->pdf->setX(141);
+            $this->pdf->write(5, $vat . ' MwSt.');
+            $this->pdf->setX(167);
+            $this->pdf->write(5, money($amount) . ' Euro');
         }
-        if($anteilvoll !== 0){
-        $this->pdf->ln();
-        $this->pdf->setX(141);
-        $this->pdf->write(5, "16% MwSt.");
-        $this->pdf->setX(167);
-        $this->pdf->write(5, number_format($anteilvoll, 2, ',', '.').' Euro');
-        }
-        //Alt ori
-       /*  $this->pdf->setX(141);
-        $this->pdf->write(5, "zzgl. 19% MwSt.");
-        $this->pdf->setX(167);
-        $this->pdf->write(5, number_format($this->betrag - ($gesamt / 1.19), 2, ',', '.').' Euro'); */
-        //gesamt Brutto
+
         $this->pdf->setFont("Arial","B", 8);
         $this->pdf->ln();
         $this->pdf->setX(141);
         $this->pdf->write(5, "Gesamt Brutto");
         $this->pdf->setX(167);
-        $this->pdf->write(5, number_format($this->betrag, 2, ',', '.').' Euro');
+        $this->pdf->write(5, money($this->order->grossTotal).' Euro');
 
         $this->pdf->setFont("Arial","", 8);
 
-        $this->pdf->ln(20);
-        // Show proper payment text
-        if ($this->reservierung->getBarzahlung() == 1 && $this->prozent !== 20) {
-            $this->pdf->ln();
-            $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'barzahlung')));
-        } else {
-            $this->pdf->write(5, utf8_decode(sprintf($this->text->getText('rechnung', 'bezahlung'), number_format($this->betrag, 2, ',', '.'), $this->heute->modify('+'.$this->text->getText('rechnung', 'bezahlung_bis_tagen').'days')->format('d.m.Y'))));
+        // $this->pdf->ln(20);
+        // // Show proper payment text
+        // if ($this->reservierung->getBarzahlung() == 1 && $this->prozent !== 20) {
+        //     $this->pdf->ln();
+        //     $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'barzahlung')));
+        // } else {
+        //     $this->pdf->write(5, utf8_decode(sprintf($this->text->getText('rechnung', 'bezahlung'), number_format($this->betrag, 2, ',', '.'), $this->heute->modify('+'.$this->text->getText('rechnung', 'bezahlung_bis_tagen').'days')->format('d.m.Y'))));
 
-            $this->pdf->ln();
-            $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'pay_info')));
-            $this->pdf->ln();
-        }
+        //     $this->pdf->ln();
+        //     $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'pay_info')));
+        //     $this->pdf->ln();
+        // }
 
-        $this->pdf->ln();
-        $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'agb')));
-        $this->pdf->ln();
-        $this->pdf->ln();
-        $this->pdf->ln();
-        $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg')));
-        $this->pdf->ln();
-        $this->pdf->ln();
-        $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg_name')));
-        $this->pdf->ln();
-        $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg_bezeichnung')));
-        $this->pdf->ln();
-        $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg_firma')));
+        // $this->pdf->ln();
+        // $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'agb')));
+        // $this->pdf->ln();
+        // $this->pdf->ln();
+        // $this->pdf->ln();
+        // $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg')));
+        // $this->pdf->ln();
+        // $this->pdf->ln();
+        // $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg_name')));
+        // $this->pdf->ln();
+        // $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg_bezeichnung')));
+        // $this->pdf->ln();
+        // $this->pdf->write(5, utf8_decode($this->text->getText('rechnung', 'mfg_firma')));
 
     }
 
@@ -368,10 +284,5 @@ class Pdf
         // $this->pdf->write(5, utf8_decode($blocks['company']));
         // $this->pdf->setX(150);
         // $this->pdf->write(5, utf8_decode($blocks['company']));
-    }
-
-    public function output($type = 'I', $dest = '')
-    {
-        return $this->pdf->output($type, $dest);
     }
 }
