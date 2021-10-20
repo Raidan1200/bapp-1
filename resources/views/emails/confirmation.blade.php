168e515dd89d092f768640ae178e539e074dd5de
Oliver Schumacher <15702230+birusch@users.noreply.github.com>
2021-10-20 19:02:09 +0200

    Add date to confirmation email

--- resources/views/emails/confirmation.blade.php
+++ resources/views/emails/confirmation.blade.php
@@ -3,7 +3,11 @@
 
   <p class="my-2">Vielen Dank für ihr Buchung bei {{ $order->venue->name }}.</p>
 
-  <p class="my-2">Ihre Anzahlung für die gebuchte Reservierung haben wir erhalten und bestätigen hiermit verbindlich ihre Reservierung.</p>
+  <p class="my-2">
+    Ihre Anzahlung für die gebuchte Reservierung haben wir erhalten
+    und bestätigen hiermit verbindlich ihre Reservierung für den
+    {{ $order->starts_at->timezone('Europe/Berlin')->format('d.m.Y') }}.
+  </p>
 
   <p class="my-2">
     Sollte sich noch etwas bezüglich der Personenanzahl ändern,
