50bc139a0a8715b4974cecb3fc2c50a1c24bd8ea
Oliver Schumacher <15702230+birusch@users.noreply.github.com>
2021-10-20 18:50:29 +0200

    Ficken 2

--- resources/views/emails/footer.blade.php
+++ resources/views/emails/footer.blade.php
@@ -1,5 +1,7 @@
   <p class="mt-6">
-    Bitte beachten Sie unsere <a href="https://www.huettenzauber-dresden.de/index.php?page=agb">Allgemeinen Geschäftsbedingungen</a>.
+    Bitte beachten Sie unsere <a
+      class="hover:text-blue-500"
+      href="https://www.huettenzauber-dresden.de/index.php?page=agb">Allgemeinen Geschäftsbedingungen</a>.
   </p>
 </body>
 </html>
