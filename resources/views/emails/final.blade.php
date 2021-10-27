b72ecde78f13f9b0018327d57ec2d1e0785792b7
Raidan1200 <93259644+Raidan1200@users.noreply.github.com>
2021-10-27 13:58:31 +0200

    Update final.blade.php

--- resources/views/emails/final.blade.php
+++ resources/views/emails/final.blade.php
@@ -1,7 +1,7 @@
 <x-email-layout>
   <h1 class="text-2xl">Ihre Gesamtrechnung</h1>
 
-  <p class="my-2">Vielen Dank für Ihre Buchung bei {{ $order->venue->name }}.</p>
+  <p class="my-2">Vielen Dank für Ihre Buchung beim {{ $order->venue->name }}.</p>
 
   <p class="my-2">
     Bitte überweisen Sie {{ money($order->deposit_amount) }}€
